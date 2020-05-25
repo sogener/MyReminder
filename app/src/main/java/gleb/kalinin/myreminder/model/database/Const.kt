8953238941754239462f8c0e7155b26ee4d4bbbd
dedushkaded <glebstuck@yandex.ru>
2020-05-25 21:00:18 +0500

    Создал DBHandler(adapter) SQLite, и Const (хранящая переменные SQLite)

---
+++ app/src/main/java/gleb/kalinin/myreminder/model/database/Const.kt
@@ -0,0 +1,13 @@
+package gleb.kalinin.myreminder.model.database
+
+const val DB_NAME = "ToDoList"
+const val DB_VERSION = 1
+const val TABLE_TODO = "ToDo"
+const val COL_ID = "id"
+const val COL_CREATED_AT = "createdAt"
+const val COL_NAME = "name"
+
+const val TABLE_TODO_ITEM = "ToDoItem"
+const val COL_TO_DO_ID = "toDoId"
+const val COL_ITEM_NAME = "itemName"
+const val COL_IS_COMPLETED = "isCompleted"
